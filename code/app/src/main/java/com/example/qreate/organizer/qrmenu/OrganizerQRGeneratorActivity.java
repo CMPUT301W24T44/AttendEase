ebee05e400d53c733d3b5dd68f79bd40e90b64c0
Denis Soh <dsoh@ualberta.ca>
2024-03-29 00:07:18 -0600

    ALL The spinner work i did before
    like 7 commits of past work

--- code/app/src/main/java/com/example/qreate/organizer/qrmenu/OrganizerQRGeneratorActivity.java
+++ code/app/src/main/java/com/example/qreate/organizer/qrmenu/OrganizerQRGeneratorActivity.java
@@ -1,9 +1,11 @@
 package com.example.qreate.organizer.qrmenu;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -15,6 +17,12 @@ import android.widget.Spinner;
 import com.example.qreate.R;
 import com.example.qreate.organizer.qrmenu.OrganizerEvent;
 import com.example.qreate.organizer.qrmenu.OrganizerEventSpinnerArrayAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
@@ -22,6 +30,8 @@ import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The following class is responsible for the qr generator screen and functionality
  *
@@ -31,6 +41,7 @@ import java.util.ArrayList;
 public class OrganizerQRGeneratorActivity extends AppCompatActivity {
     ArrayList<OrganizerEvent> events;
     Spinner eventsSpinner;
+    private FirebaseFirestore db;
     OrganizerEventSpinnerArrayAdapter eventSpinnerArrayAdapter;
 
 
@@ -45,6 +56,7 @@ public class OrganizerQRGeneratorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.organizer_generate_qr_code_screen);
+        db = FirebaseFirestore.getInstance();
 
         ImageButton backButton = findViewById(R.id.generate_qr_code_screen_backbutton);
         Button createCodesButton = findViewById(R.id.generate_qr_code_confirmbutton);
@@ -79,7 +91,6 @@ public class OrganizerQRGeneratorActivity extends AppCompatActivity {
 
             }
         });
-        addEventsInit();
 
         eventSpinnerArrayAdapter.setDropDownViewResource(R.layout.organizer_event_list_recycler_row_layout);
 
@@ -126,11 +137,62 @@ public class OrganizerQRGeneratorActivity extends AppCompatActivity {
 
     //Temporary to test swap this with the firebase data
     private void addEventsInit(){
-        String []cities ={"Edmonton", "Vancouver", "Toronto", "Hamilton", "Denver", "Los Angeles"};
-        String []provinces = {"AB", "BC", "ON", "ON", "CO", "CA"};
-        for(int i=0;i<cities.length;i++){
-            events.add((new OrganizerEvent(cities[i], provinces[i],"date", "doesnt work here")));
-        }
+
+
+        // TODO THIS CODE CRASHES IF THERES NO DETAIL OR DATE SO I COMMENTED IT OUT UNCOMMENT WHEN DATA IS FIXED
+        String device_id = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+        db.collection("Organizers")
+                .whereEqualTo("device_id", device_id)
+                .limit(1)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            QuerySnapshot querySnapshot = task.getResult();
+                            if (!querySnapshot.isEmpty()) {
+
+
+                                // Since the unique ID is unique, we only expect one result
+                                DocumentSnapshot document = querySnapshot.getDocuments().get(0);
+
+
+                                List<DocumentReference> referenceArray = (List<DocumentReference>) document.get("events_list");
+
+
+                                //assert createdEvents != null;
+                                for (DocumentReference reference : referenceArray) {
+                                    reference.get().addOnCompleteListener(referencedTask -> {
+                                        if (referencedTask.isSuccessful()) {
+                                            DocumentSnapshot referencedDocument = referencedTask.getResult();
+                                            if (referencedDocument.exists()) {
+                                                //TODO description/dates are not set in most firebase stuff this will cause it to crash
+                                                String eventName = referencedDocument.getString("name");
+                                                //String eventDetails = document.getString("description");
+                                                //String eventDate = document.getString("date");
+                                                String eventOrganizer = referencedDocument.getString("organizer");
+                                                String eventID = referencedDocument.getId();
+                                                events.add(new OrganizerEvent(eventName, "details", "date", eventOrganizer, eventID));
+                                            } else {
+                                                System.out.println("Referenced document does not exist");
+                                            }
+                                        } else {
+                                            System.out.println("Error fetching referenced document: " + referencedTask.getException());
+                                        }
+                                    });
+                                }
+
+
+
+
+                            } else {
+                                Log.d("Firestore", "No such document");
+                            }
+                        } else {
+                            Log.d("Firestore", "get failed with ", task.getException());
+                        }
+                    }
+                });
     }
     /**
      * generates bitmap of qr code based on string
